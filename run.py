92384d3cc8d1386a9bcb50738ce6e90b930a5394
Amrutha2103 <amrutha.ananthan@outlook.com>
2023-03-02 21:37:40 +0000

    Initialize the game in Init method and start the game loop in play method

--- run.py
+++ run.py
@@ -1,20 +1,26 @@
 import random
 class JumbledWordGame:
     def __init__(self, wordlist):
-
-
+        self.wordlist = wordlist
+        self.score = 0
+        self.lives = 3
 
     def play(self):
-
-
-
-
+        word = random.choice(self.wordlist)
+        jumbled = self.jumble(word)
+        print(f"Jumbled word: {jumbled}")
+        guess = input("Guess the word: ")
+        if guess == word:
+            print("Correct!")
+            self.score += 1
+        else:
+            print(f"Incorrect. The word was {word}.")
+            self.lives -= 1
+        print(f"Score: {self.score}  Lives: {self.lives}\n")
+        
 
     def jumble(self, word):
-
-
-
-
+        
 
 def load_wordlist(filename):
 
