10b1132fb05b70f962c305606256e7fb1931b57b
Amrutha2103 <amrutha.ananthan@outlook.com>
2023-03-02 21:20:07 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,37 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+class JumbledWordGame:
+    def __init__(self, wordlist):
+
+
+
+    def play(self):
+
+
+
+
+
+    def jumble(self, word):
+
+
+
+
+
+def load_wordlist(filename):
+
+
+
+
+def show_instructions():
+
+
+
+
+def play_game(wordlist):
+
+
+
+
+
+def main():
+
+
