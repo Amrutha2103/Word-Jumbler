e8afad3fe0408ded0ecc1f9ae3e20c935f7a7036
Amrutha2103 <amrutha.ananthan@outlook.com>
2023-03-03 10:39:01 +0000

    Add shuffle words to the list

--- run.py
+++ run.py
@@ -1,28 +1,32 @@
 import random
+
+
 class JumbledWordGame:
-    def __init__(self, wordlist):
+    def __init__(self):
         self.wordlist = ['python', 'java', 'ruby', 'javascript', 'html', 'css']
         self.score = 0
         self.lives = 3
 
     def play(self):
-        word = random.choice(self.wordlist)
+        word = self.wordlist[0]
+        self.wordlist.pop(0)
         jumbled = self.jumble(word)
         print(f"Jumbled word: {jumbled}")
         guess = input("Guess the word: ")
-        if guess == word:
+        if guess.lower() == word.lower():
             print("Correct!")
             self.score += 1
         else:
             print(f"Incorrect. The word was {word}.")
             self.lives -= 1
+            self.wordlist.append(word)
         print(f"Score: {self.score}  Lives: {self.lives}\n")
-        
 
     def jumble(self, word):
         jumbled = list(word)
         random.shuffle(jumbled)
         return ''.join(jumbled)
+
 
 def show_instructions():
     print("Welcome to the jumbled word game!")
@@ -32,28 +36,29 @@ def show_instructions():
     print("Your score will increase by one for each correct guess.")
     print("Good luck!\n")
 
-def play_game(wordlist):
+
+def play_game():
     game = JumbledWordGame()
-    while game.lives > 0:
+    random.shuffle(game.wordlist)
+    while game.lives > 0 and len(game.wordlist) > 0:
         game.play()
         if game.lives == 0:
             print("Game over!")
             break
-        play_again = input("Play again? (y/n): ")
-        if play_again.lower() != 'y':
-            break
-        else:
-            print("Starting a new game...")
-            game.score = 0
-            game.lives = 3
-    print(f"Final score: {game.score}")
+    play_again = input("Play again? (y/n): ")
+    if play_again.lower() != 'y':
+        print(f"Final score: {game.score}")
+    else:
+        print("Starting a new game...")
+        game.score = 0
+        game.lives = 3
+        main()
+
 
 def main():
-    wordlist = ["python", "java", "ruby", "javascript", "html", "css"]
     show_instructions()
     play_game()
 
+
 if __name__ == '__main__':
     main()
-
-
