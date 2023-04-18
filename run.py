68b213a116ada5bb25bf808936e4fcb78972c88d
Amrutha2103 <amrutha.ananthan@outlook.com>
2023-04-18 16:36:09 +0000

    Solve invalid data entry and error handling

--- run.py
+++ run.py
@@ -1,9 +1,13 @@
 import random
 import time
+import os
+
+
+def clear():
+    os.system("cls" if os.name == "nt" else "clear")
+
 
 # Defining a class called JumbledWordGame
-
-
 class JumbledWordGame:
     def __init__(self):
         self.wordlist = ['python', 'java', 'ruby', 'javascript', 'html', 'css']
@@ -21,12 +25,19 @@ class JumbledWordGame:
 # Start the timer
 
         start_time = time.time()
-
-        guess = input("Guess the word: ")
+        while True:
+            guess = input("Guess the word: ")
+            if len(guess) != len(word):
+                print("Your guess is invalid")
+            elif not guess.isalpha():
+                print("Your guess must be only letters")
+            else:
+                break
 
 # Stop the timer
 
         end_time = time.time()
+        clear()
 
         if guess.lower() == word.lower():
             print("Correct!")
@@ -43,7 +54,10 @@ class JumbledWordGame:
 
     def jumble(self, word):
         jumbled = list(word)
-        random.shuffle(jumbled)
+        while True:
+            random.shuffle(jumbled)
+            if ''.join(jumbled) != word:
+                break
         return ''.join(jumbled)
 
 
@@ -67,12 +81,15 @@ def play_game():
     while game.lives > 0 and len(game.wordlist) > 0:
         game.play()
         if game.lives == 0:
+            clear()
             print("Game over!")
             break
     play_again = input("Play again? (y/n): ")
     if play_again.lower() != 'y':
+        clear()
         print(f"Final score: {game.score}")
     else:
+        clear()
         print("Starting a new game...")
         game.score = 0
         game.lives = 3
@@ -87,4 +104,5 @@ def main():
 
 
 if __name__ == '__main__':
+    clear()
     main()
